e69a0e3190911eda86eab5d1726c0ae30faa02fd
dwipaalfarisi <dwipa.alfarisi@gmail.com>
2022-10-07 11:33:01 +0700

    almost done pmf

--- probability/functuser.py
+++ probability/functuser.py
@@ -1,2 +1,20 @@
 def print_format(data, format):
+    '''
+    Format a number's digit
+    '''
     print(f"{data:.{format}f}")
+
+def addlabels(x,y):
+    '''
+    Add labels to the given bar chart
+    '''
+    for i in range(len(x)):
+        plt.text(i, y[i]//2, y[i], ha = 'center')
+
+def prob(A):
+    """Computes the probability of a proposition, A."""    
+    return A.mean()
+
+def conditional(proposition, given):
+    """Probability of A conditioned on given."""
+    return prob(proposition[given])
